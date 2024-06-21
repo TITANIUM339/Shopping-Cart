311eeaca5b8236a18c97dc30e2aa41a98b95eabc
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-06-21 20:04:09 +0300

    Add links

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom";
+import classes from "../styles/Categories.module.css";
+
 function Categories() {
-    return <h1>Categories</h1>;
+    return (
+        <section className={classes.container}>
+            <h1>Categories</h1>
+            <nav className={classes.linksContainer}>
+                <Link to="/shop" className={classes.link}>
+                    All products
+                </Link>
+                <Link to="jewelry" className={classes.link}>
+                    Jewelry
+                </Link>
+                <Link to="electronics" className={classes.link}>
+                    Electronics
+                </Link>
+                <Link to="mens-clothing" className={classes.link}>
+                    Men&apos;s clothing
+                </Link>
+                <Link to="womens-clothing" className={classes.link}>
+                    Women&apos;s clothing
+                </Link>
+            </nav>
+        </section>
+    );
 }
 
 export default Categories;
