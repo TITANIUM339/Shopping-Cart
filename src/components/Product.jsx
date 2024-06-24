e4e0febbedcfbe07f94a7f8593efbc6ab2a71ae2
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-06-24 11:40:30 +0300

    Remove files

--- src/components/Product.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router-dom";
-
-function Product() {
-    const { id } = useParams();
-
-    return <h1>{id}</h1>;
-}
-
-export default Product;
--- src/tests/Hello.test.jsx
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import Hello from "../components/Hello.jsx";
-
-test.skip("renders h1 element with Hello", () => {
-    render(<Hello />);
-
-    expect(screen.getByRole("heading", { name: "Hello" })).toBeInTheDocument();
-});
