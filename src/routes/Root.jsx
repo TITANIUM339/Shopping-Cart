57efb23f4ec89fb34a43d2d0f78f9fcf58dd2b9f
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-06-26 12:03:43 +0300

    Add toast container and cart state

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,15 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useImmer } from "use-immer";
+import { getTotalItemsInCart } from "../helpers.js";
+import { ToastContainer } from "react-toastify";
 
 function Root() {
+    const [cart, setCart] = useImmer([]);
+
     return (
         <>
-            <Header />
+            <Header cartItemsCount={getTotalItemsInCart(cart)} />
             <main>
-                <Outlet />
+                <Outlet context={[cart, setCart]} />
             </main>
             <Footer />
+            <ToastContainer />
         </>
     );
 }
