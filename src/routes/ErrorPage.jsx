b95d11c5392a9e13c815ccb0ac07d0e57b0df705
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-06-28 07:19:46 +0300

    Finish ErrorPage route

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,5 +1,13 @@
+import { Warning } from "phosphor-react";
+import classes from "../styles/ErrorPage.module.css";
+
 function ErrorPage() {
-    return <h1>An error has occurred</h1>;
+    return (
+        <div className={classes.container}>
+            <h1 className={classes.error}><Warning weight="bold"/> Error</h1>
+            <p>It seems that the page you were looking for does not exist.</p>
+        </div>
+    );
 }
 
 export default ErrorPage;
