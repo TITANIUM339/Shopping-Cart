90d94038e9c38d7fe95bf87b3ac42c0748787126
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-06-28 08:01:40 +0300

    Add back link

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,11 +1,21 @@
 import { Warning } from "phosphor-react";
 import classes from "../styles/ErrorPage.module.css";
+import { Link } from "react-router-dom";
 
 function ErrorPage() {
     return (
         <div className={classes.container}>
-            <h1 className={classes.error}><Warning weight="bold"/> Error</h1>
-            <p>It seems that the page you were looking for does not exist.</p>
+            <section className={classes.errorContainer}>
+                <h1 className={classes.error}>
+                    <Warning weight="bold" /> Error
+                </h1>
+                <p>
+                    It seems that the page you were looking for does not exist.
+                </p>
+                <Link to="/" className={classes.back}>
+                    Go back
+                </Link>
+            </section>
         </div>
     );
 }
